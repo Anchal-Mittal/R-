d853028a951ad4b9109e3375d0ee0b76d85d7ce2
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-10 13:34:52 +0530

    matrix arithmatic

---
+++ matrix/m3.R
@@ -0,0 +1,28 @@
+m1<-matrix(c(3,34,2,4,2,5), nrow=2)
+m2<-matrix(c(2,3,4,5,1,2), nrow=2)
+
+add_result=m1+m2
+print(add_result)
+
+sub_result=m1-m2
+print(sub_result)
+
+m3<-matrix(c(3,34,2,4,2,5), nrow=2)
+m4<-matrix(c(2,3,4,5,1,2), nrow=2)
+
+mul_result <-m3*m4
+print(mul_result)
+
+div_result <- m3/m4
+print(div_result)
+
+if(FALSE){
+"ERROR DIMENSION SHOULD SAME
+m3<-matrix(c(3,34,2,4,5), nrow=2)
+m4<-matrix(c(2,3,4,5,1,2), nrow=2)
+
+
+add_result=m1+m2
+print(add_result)"
+}
+
