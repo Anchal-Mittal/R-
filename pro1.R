6b5b59666780d25afca77c595c018c333e9d15f2
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-10 11:31:43 +0530

    hello World with R AND TRIC FOR MULTILINE COMMENT

---
+++ pro1.R
@@ -0,0 +1,8 @@
+myString<- "HELLO WORLD"
+print (myString)
+
+
+#============================DOES NOT ALLOW MULTILINE COMMENTS SO , APPLY SOME TRICS=================================================== 
+
+if(FALSE)
+" FIRST HELLO WORLD PROGRAM"
