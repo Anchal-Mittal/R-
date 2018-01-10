8c924f97542c64f03d117fd683ceb6cee494bafd
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-10 12:16:07 +0530

    creating a sequence

---
+++ pro4.R
@@ -0,0 +1,12 @@
+# creating a sequence 
+v <- 1:10
+print(v)
+
+# creating a sequence of even number
+v <- seq(0,10, by=2)
+print (v)
+
+# creating a sequence of odd number
+v <- seq(1,10, by=2)
+print (v)
+
