536ba0ca58ce4a95f8dcf3d3dfe68384b35e7b94
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-10 12:35:32 +0530

    vector arithmetic

---
+++ pro6.R
@@ -0,0 +1,21 @@
+v1<-c(2 ,3 ,4 ,5,6)
+v2 <-c(3,6,1,2,4)
+
+#addition of two vectors
+add_result <- v1+v2
+print(add_result)
+
+#subtraction of two vectors
+sub_result <- v1-v2
+print(sub_result)
+
+#multipication of two vectors
+mul_result <- v1*v2
+print(mul_result)
+
+#dividation of two vectors
+div_result <- v1/v2
+print(div_result)
+
+if(FALSE)
+"Two vectors of same length can be added, subtracted, multiplied or divided giving the result as a vector output."
